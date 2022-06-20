53f9e3ededcd8ceced90316b5b9c14d8d84ab064
Jan Gergel <jan.gergel@educhem.cz>
2022-06-20 13:27:59 +0200

    Konec závěrečky

--- Závěrečný projekt/Program.cs
+++ Závěrečný projekt/Program.cs
@@ -6,7 +6,7 @@ int cislo = generatorVoleb.Next(1,4);
 
 Console.WriteLine("Ahoj vítej v mé hře kámen-nůžky-papír doufám že si hru užiješ a vyhraješ nad pc (Jinak si u mě bot xdddddd)");
 
-
+#region začátek
 Console.WriteLine("Vitej ve hře kámen - nůžky  - papír");
 Console.WriteLine("Hra začíná za");
 Console.WriteLine("3");
@@ -15,106 +15,125 @@ Console.WriteLine("2");
 Thread.Sleep(1000);
 Console.WriteLine("1");
 Console.Clear();
-
-int SkoreHrac = 0;
+#endregion začátek
+bool hraVProcesu = true;
+int SkoreHrac = 0; 
 int SkoreNepritel = 0;
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine("Hrac: 0");
-Console.ForegroundColor = ConsoleColor.Blue;
-Console.WriteLine("Nepritel: 0");
-Console.ForegroundColor = ConsoleColor.White;
-
-Console.WriteLine("---------");
-
-Console.WriteLine(" Kolo hráče:");
-Console.WriteLine("1 - kámen");
-Console.WriteLine("2 - nůžky");
-Console.WriteLine("3 - papír");
-
-Console.WriteLine("---------");
-
-string Hrac = Console.ReadLine();
-
-if (Hrac == "1")
+#region while a if
+while (hraVProcesu == true)
 {
-    if (cislo == 3)
-    {
-        Console.WriteLine("Nepřítel Vyhraje");
-    }
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Hrac: 0");
+    Console.ReadKey();
+    Console.Clear();
+  
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Hrac:{SkoreHrac} ");
     Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Nepritel: 1");
-    Console.ForegroundColor = ConsoleColor.White;
-    if (cislo == 2)
-    {
-        Console.WriteLine("Vyhrál jsi");
-    }
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Hrac: 1");
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Nepritel: 1");
+    Console.WriteLine($"Nepritel:{SkoreNepritel} ");
     Console.ForegroundColor = ConsoleColor.White;
 
-    if (cislo == 1)
-    {
-        Console.WriteLine("Ani jeden remíza");
-    }
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Hrac: 1");
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Nepritel: 1");
-    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("---------");
 
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(" Kolo hráče:");
+    Console.WriteLine("1 - kámen");
+    Console.WriteLine("2 - nůžky");
+    Console.WriteLine("3 - papír");
+
+    Console.WriteLine("---------");
+
+    string Hrac = Console.ReadLine();
+
+    if (Hrac == "1")
+    {
+        if (cislo == 3)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Nepřítel Vyhraje");
+            ++SkoreNepritel;
+        }
+    
+        if (cislo == 2)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Vyhrál jsi");
+            ++SkoreHrac;
+        }
+
+        
+     
+
+        if (cislo == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Ani jeden remíza");
+        }
+
+
+    }
+    if (Hrac == "2")
+    {
+        if (cislo == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Prohrál jsi");
+            ++SkoreNepritel;
+        }
+   
+
+        if (cislo == 2)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Remíza a nůžky se stupily xdddd");
+        }
+  
+
+        if (cislo == 3)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Vyhrál jsi, nůžky papír rozstřihly");
+            ++SkoreHrac;
+        }
+    
+
+    }
+    if (Hrac == "3")
+    {
+        if(cislo == 1)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Vyhrál jsi a zabalil kámen");
+                ++SkoreHrac;
+        }
+        if(cislo == 2)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Prohrál jsi a nemáš papír xddddd");
+             ++SkoreNepritel;
+        }
+        if(cislo == 3)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Remíza a papíry se zmuchali");
+        }
+    }
+    //podmínka, když ScoreHrac == 5 NEBO ScoreEnemy == 5
+    // break;
+    #endregion while a if
+    #region if
+    if (SkoreHrac == 5 )
+    {
+        Console.WriteLine("Jsí vítěz");
+            break;
+    }
+    if(SkoreNepritel == 5)
+    {
+        Console.WriteLine("Jsi looser");
+        break;
+    }
+    #endregion if
 }
-if (Hrac == "2")
-{
-    if (cislo == 1)
-    {
-        Console.WriteLine("Prohrál jsi");
-    }
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Hrac: 1");
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Nepritel: 2");
-    Console.ForegroundColor = ConsoleColor.White;
+//pokračování po přerušení cyklu while
 
-    if (cislo == 2)
-    {
-        Console.WriteLine("Remíza a nůžky se stupily xdddd");
-    }
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Hrac: 1");
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Nepritel: 2");
-    Console.ForegroundColor = ConsoleColor.White;
-
-    if (cislo == 3)
-    {
-        Console.WriteLine("Vyhrál jsim, nůžky papír rozstřihly");
-    }
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Hrac: 2");
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("Nepritel: 2");
-    Console.ForegroundColor = ConsoleColor.White;
-
-}
-if (Hrac == "3")
-{
-    if(cislo == 1)
-    {
-        Console.WriteLine("Vyhrál jsi a zabalil kámen");
-    }
-    if(cislo == 2)
-    {
-        Console.WriteLine("Prohrál jsi a nemáš papír xddddd");
-    }
-    if(cislo == 3)
-    {
-        Console.WriteLine("Remíza a papíry se zmuchali");
-    }
-}
 
 
 
