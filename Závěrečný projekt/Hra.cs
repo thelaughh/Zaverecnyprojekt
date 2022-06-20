31c722d56b5c8fedf0b69fda7516cd04de753718
Jan Gergel <jan.gergel@educhem.cz>
2022-06-20 09:08:12 +0200

    Přidejte soubory projektu.

---
+++ Závěrečný projekt/Hra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Závěrečný_projekt
+{
+    internal class Hra
+    {
+        public int SkoreHrac { get; set; }
+        public int SkoreNepritel { get; set; }
+
+        public Hra()
+        {
+            SkoreHrac = 0;
+            SkoreNepritel = 0;
+        }
+
+    }
+}
---
+++ Závěrečný projekt/Program.cs
@@ -0,0 +1,122 @@
+﻿using Závěrečný_projekt;
+
+
+Random generatorVoleb= new Random();
+int cislo = generatorVoleb.Next(1,4);
+
+Console.WriteLine("Ahoj vítej v mé hře kámen-nůžky-papír doufám že si hru užiješ a vyhraješ nad pc (Jinak si u mě bot xdddddd)");
+
+
+Console.WriteLine("Vitej ve hře kámen - nůžky  - papír");
+Console.WriteLine("Hra začíná za");
+Console.WriteLine("3");
+Thread.Sleep(1000);
+Console.WriteLine("2");
+Thread.Sleep(1000);
+Console.WriteLine("1");
+Console.Clear();
+
+int SkoreHrac = 0;
+int SkoreNepritel = 0;
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Hrac: 0");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("Nepritel: 0");
+Console.ForegroundColor = ConsoleColor.White;
+
+Console.WriteLine("---------");
+
+Console.WriteLine(" Kolo hráče:");
+Console.WriteLine("1 - kámen");
+Console.WriteLine("2 - nůžky");
+Console.WriteLine("3 - papír");
+
+Console.WriteLine("---------");
+
+string Hrac = Console.ReadLine();
+
+if (Hrac == "1")
+{
+    if (cislo == 3)
+    {
+        Console.WriteLine("Nepřítel Vyhraje");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hrac: 0");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Nepritel: 1");
+    Console.ForegroundColor = ConsoleColor.White;
+    if (cislo == 2)
+    {
+        Console.WriteLine("Vyhrál jsi");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hrac: 1");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Nepritel: 1");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    if (cislo == 1)
+    {
+        Console.WriteLine("Ani jeden remíza");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hrac: 1");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Nepritel: 1");
+    Console.ForegroundColor = ConsoleColor.White;
+
+}
+if (Hrac == "2")
+{
+    if (cislo == 1)
+    {
+        Console.WriteLine("Prohrál jsi");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hrac: 1");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Nepritel: 2");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    if (cislo == 2)
+    {
+        Console.WriteLine("Remíza a nůžky se stupily xdddd");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hrac: 1");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Nepritel: 2");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    if (cislo == 3)
+    {
+        Console.WriteLine("Vyhrál jsim, nůžky papír rozstřihly");
+    }
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Hrac: 2");
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Nepritel: 2");
+    Console.ForegroundColor = ConsoleColor.White;
+
+}
+if (Hrac == "3")
+{
+    if(cislo == 1)
+    {
+        Console.WriteLine("Vyhrál jsi a zabalil kámen");
+    }
+    if(cislo == 2)
+    {
+        Console.WriteLine("Prohrál jsi a nemáš papír xddddd");
+    }
+    if(cislo == 3)
+    {
+        Console.WriteLine("Remíza a papíry se zmuchali");
+    }
+}
+
+
+
+
+
